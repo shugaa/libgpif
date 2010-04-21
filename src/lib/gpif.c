a24be9caf69908838f50dfa22218c8e942865f09
Bjoern <bjoern@shugaa.de>
2010-04-21 21:24:45 +0200

    Build system improvements / Refactoring / License BSD->MIT

--- src/lib/gpif.c
+++ src/lib/gpif.c
@@ -1,30 +1,32 @@
 /*
-* Copyright (c) 2009, Björn Rehm (bjoern@shugaa.de)
-* All rights reserved.
+* Copyright (c) 2008, Björn Rehm (bjoern@shugaa.de)
 * 
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
 * 
-*  * Redistributions of source code must retain the above copyright notice, this
-*    list of conditions and the following disclaimer.
-*  * Redistributions in binary form must reproduce the above copyright notice,
-*    this list of conditions and the following disclaimer in the documentation
-*    and/or other materials provided with the distribution.
-*  * Neither the name of the author nor the names of its contributors may be
-*    used to endorse or promote products derived from this software without
-*    specific prior written permission.
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
 * 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #include "gpif.h"
 
@@ -39,6 +41,7 @@
 /* ######################################################################### */
 /*                           Private interface (Module)                      */
 /* ######################################################################### */
+
 static ssize_t prv_gpif_read(int fd, void* vptr, ssize_t len);
 static ssize_t prv_gpif_write(int fd, const void* ptr, size_t len);
 
@@ -90,8 +93,7 @@ int gpif_init(gpif_session_t *session, char *const argv[])
         return 1;
     }
 
-    /* Parent. 
-     * TODO: We need to make sure the exec step in the child succeeded */
+    /* Parent. TODO: Make sure everything is OK in the child. */
 
     /* Close unused pipe ends */
     close(pipe_togp[0]);
@@ -235,10 +237,11 @@ ssize_t prv_gpif_write(int fd, const void* ptr, size_t len)
     size_t nleft;
     ssize_t nwritten;
     fd_set waitforwrite;
+    unsigned char *byteptr = (unsigned char*)ptr;
 
     nleft = len;
     while(nleft > 0) {
-        if((nwritten = write(fd, ptr, nleft)) <= 0) {
+        if((nwritten = write(fd, (const void*)byteptr, nleft)) <= 0) {
 
             if(errno == EINTR)
                 nwritten = 0;
@@ -259,7 +262,7 @@ ssize_t prv_gpif_write(int fd, const void* ptr, size_t len)
             }
         }
         nleft -= nwritten;
-        ptr += nwritten;
+        byteptr += nwritten;
     }
 
     return len;
